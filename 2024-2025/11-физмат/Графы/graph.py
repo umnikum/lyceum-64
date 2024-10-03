ddd99f564915e566ed2a4a09139e44db1bd05467
Ilia <72198091+umnikum@users.noreply.github.com>
2024-10-03 11:24:27 +0600

    Обновление визуализации

--- 2024-2025/11-физмат/Графы/graph.py
+++ 2024-2025/11-физмат/Графы/graph.py
@@ -5,6 +5,10 @@ for line in file:
 	data.append([int(elem) for elem in elements])
 file.close()
 
+file = open('cities.dat', 'r')
+cities = [line.strip() for line in file]
+file.close()
+
 def print_table(data):
 	for row in data:
 		line = ''
@@ -19,4 +23,16 @@ def symmetrize(data):
 			data[i][j] = data[j][i]
 	return data
 
+
+def print_way(way):
+	line = str(cities[way[0]])
+	for i in range(1, len(way)):
+		line += ' -(' + str(data[way[i-1]][way[i]]) + ')-> '+ cities[way[i]]
+	print(line)
+
 print_table(symmetrize(data))
+
+way = [0, 1, 2]
+print_way(way)
+
+
