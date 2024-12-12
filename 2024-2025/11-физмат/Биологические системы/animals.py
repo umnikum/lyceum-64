5e803223f25c4cf1a3c83ec47f2c52a5b3326cc2
Ilia <72198091+umnikum@users.noreply.github.com>
2024-12-12 16:45:08 +0600

    Добавление класса Field(поле)
    Теперь животные удобно резвятся на полянке с травкой. Поле умеет:
    1) Рисовать себя
    2) Выращивать траву самостоятельно
    3) Добавлять животных на пустые клетки самостоятельно
    4) Прокручивать время на заданный интервал

--- 2024-2025/11-физмат/Биологические системы/animals.py
+++ 2024-2025/11-физмат/Биологические системы/animals.py
@@ -1,7 +1,7 @@
 from random import randint
 
 n = 10
-def coor_range(coor):
+def _coor_range(coor):
 	_min = coor - 1
 	if _min < 0:
 		_min = 0
@@ -10,51 +10,98 @@ def coor_range(coor):
 		_max = n-1
 	return range(_min, _max)
 
-def single_index(x, y):
-	return n*y + x
-
 class Cell:
-	def __init__(self, x, y, food, animal = None):
+	def __init__(self, x, y, food = 0, has_animal = False):
 		self.x = x
 		self.y = y
 		self.food = food
-		self.animal = animal
+		self.has_animal = has_animal
+	def __eq__(self, cell):
+		return self.x == cell.x and self.y == cell.y
 
-animals = []
 class Animal:
-	def __init__(self, fat, gen = None):
+	def __init__(self, fat, cell = None, gen = None):
 		self.gen = gen
 		self.fat = fat
-	def eat(self, food):
-		self.fat += 2
+		self.cell = cell
+	def is_alive(self):
+		return self.fat >= 0
+	def eat(self):
+		if self.cell.food > 0 and self.is_alive():
+			self.fat += 2
+			self.cell.food -= 1
 	def metabolism(self):
 		self.fat -= 1
-		if self.fat < 0:
-			animals.remove(self)
-field = []
-for index in range(n*n):
-	field.append(Cell(index%n, index//n, 0))
 
-def grow_food(gen_amount):
-	for i in range(gen_amount):
-		x = randint(0, n-1)
-		y = randint(0, n-1)
-		field[single_index(x,y)].food += 1
+class Field:
+	def __init__(self, size):
+		self.size = size
+		self.cells = []
+		self.animals = []
+		for y in range(size):
+			line = []
+			for x in range(n):
+				line.append(Cell(x,y))
+			self.cells.append(line)
+	def print(self):
+		for y in range(self.size):
+			line = ''
+			for x in range(self.size):
+				cell = self.cells[y][x]
+				if cell.has_animal:
+					animal = self.find_animal(cell)
+					if animal != None:
+						if animal.is_alive():
+							line += '^o^'
+						else:
+							line += 'x_x'
+				elif cell.food > 0:
+					line += '\\#/'
+				else:
+					line += '___'
+			print(line)
+	def _coor_range(coor):
+		_min = coor - 1
+		if _min < 0:
+			_min = 0
+		_max = coor + 1
+		if _max >= self.size:
+			_max = self.size-1
+		return range(_min, _max)
+	def grow_food(self, amount):
+		for i in range(amount):
+			x = randint(0, self.size-1)
+			y = randint(0, self.size-1)
+			self.cells[y][x].food += 1
+	def put_animals(self, animals):
+		for animal in animals:
+			free_cell = False
+			while not free_cell:
+				x = randint(0, self.size-1)
+				y = randint(0, self.size-1)
+				if self.cells[y][x].has_animal == False:
+					free_cell = True
+			animal.cell = self.cells[y][x]
+			self.animals.append(animal)
+			self.cells[y][x].has_animal = True
+	def find_animal(self, cell):
+		for animal in self.animals:
+			if animal.cell == cell:
+				return animal
+		return None
+	def spend_time(self, time):
+		for t in range(time):
+			for animal in self.animals:
+				animal.eat()
+				animal.metabolism()
 
+field = Field(10)
+field.grow_food(50)
 
-def print_field():
-	for y in range(n):
-		line = ''
-		for x in range(n):
-			cell = field[single_index(x,y)]
-			if cell.animal != None:
-				line += '@'
-			elif cell.food > 0:
-				line += '#'
-			else:
-				line += ' '
-			line += '\t'
-		print(line)
+animals = []
+for i in range(10):
+	animals.append(Animal(2))
+field.put_animals(animals)
 
-grow_food(n*n//2)
-print_field()
+field.spend_time(3)
+field.print()
